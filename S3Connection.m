05b7a326b4160259b3e9386e1c71ff569c0a37f4
marc <marc@symbioticsoftware.com>
2018-07-17 22:03:03 -0700

    More code style updates.

--- S3Connection.m
+++ S3Connection.m
@@ -45,7 +45,7 @@
 @implementation S3Connection
 
 - (id)initWithAccessKeyId:(NSString *)accessKeyId secretAccessKey:(NSString *)secretAccessKey {
-  if(self = [super init]) {
+  if (self = [super init]) {
     self.accessKeyId = accessKeyId;
     self.secretAccessKey = secretAccessKey;
   }
@@ -55,7 +55,7 @@
 - (void)dealloc {
   [_connection cancel];
   _connection = nil;
-  if(_responseData) {
+  if (_responseData) {
     [_responseData release];
     _responseData = nil;
   }
@@ -71,11 +71,11 @@
   BOOL activeConnection = (_connection != nil);
   [_connection cancel];
   _connection = nil;
-  if(_responseData) {
+  if (_responseData) {
     [_responseData release];
     _responseData = nil;
   }
-  if(activeConnection) {
+  if (activeConnection) {
     [self release];
   }
 }
@@ -92,11 +92,11 @@
   CCHmacContext ctx;
   unsigned char hmac[20];
   
-  if(!md5) md5 = @"";
-  if(!contentType) contentType = @"";
+  if (!md5) md5 = @"";
+  if (!contentType) contentType = @"";
   
   // Create the string to sign
-  if([resource hasPrefix:@"/"]) {
+  if ([resource hasPrefix:@"/"]) {
     canonicalizedResource = [NSString stringWithFormat:@"/%@%@", _bucket, resource];
   } else {
     canonicalizedResource = [NSString stringWithFormat:@"/%@/%@", _bucket, resource];
@@ -126,11 +126,11 @@
   NSString *extension;
 
   extension = [path pathExtension];
-  if([extension length]) {
+  if ([extension length]) {
     uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (CFStringRef)extension, NULL);
-    if(uti) {
+    if (uti) {
       mime = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType);
-      if(mime) {
+      if (mime) {
         contentType = [NSString stringWithString:(NSString *)mime];
         CFRelease(mime);
       }
@@ -167,8 +167,8 @@
   CC_MD5_CTX ctx;
   uint8_t hash[16];
   
-  if(![data length] || ![key length]) {
-    if(completionHandler) {
+  if (![data length] || ![key length]) {
+    if (completionHandler) {
       completionHandler([self errorWithString:STRING_S3_MISSINGPARAMS]);
     }
     return;
@@ -178,12 +178,12 @@
   [self cancelCurrentRequest];
 
   // Don't allow keys to start with a /
-  if([key hasPrefix:@"/"]) {
+  if ([key hasPrefix:@"/"]) {
     key = [key substringFromIndex:1];
   }
 
   // Calculate the MD5 and other headers
-  if(![contentType length]) {
+  if (![contentType length]) {
     contentType = [self mimeType:key];
   }
   CC_MD5_Init(&ctx);
@@ -199,7 +199,7 @@
                    resource:key];
   
   // Setup the request
-  if(options & S3_HTTPS) {
+  if (options & S3_HTTPS) {
     url = [NSURL URLWithString:[NSString stringWithFormat:S3_SECURE_URL, _bucket, key]];
   } else {
     url = [NSURL URLWithString:[NSString stringWithFormat:S3_URL, _bucket, key]];
@@ -210,38 +210,38 @@
              timeoutInterval:15.0];
   [request setHTTPMethod:@"PUT"];
   // Check for the gzip magic number and add the Content-Encoding header
-  if(options & S3_DETECT_GZIP && IS_GZIP([data bytes])) {
+  if (options & S3_DETECT_GZIP && IS_GZIP([data bytes])) {
     [request setValue:@"gzip" forHTTPHeaderField:@"Content-Encoding"];
   }
-  if(options & S3_NO_CACHE) {
+  if (options & S3_NO_CACHE) {
     [request setValue:@"no-cache" forKeyPath:@"Cache-Control"];
   }
-  if(options & S3_PERMANENT_CACHE) {
+  if (options & S3_PERMANENT_CACHE) {
     [request setValue:@"max-age=315360000" forKeyPath:@"Cache-Control"];
   }
-  if(options & S3_REDUCED_REDUNDANCY) {
+  if (options & S3_REDUCED_REDUNDANCY) {
     [request setValue:@"REDUCED_REDUNDANCY" forKeyPath:@"x-amz-storage-class"];
   }
-  if([contentType length]) {
+  if ([contentType length]) {
     [request setValue:contentType forHTTPHeaderField:@"Content-Type"];
   }
   [request setValue:[NSString stringWithFormat:@"%u", [data length]] forHTTPHeaderField:@"Content-Length"];
   [request setValue:md5 forHTTPHeaderField:@"Content-MD5"];
   [request setValue:date forHTTPHeaderField:@"Date"];
   [request setValue:authorization forHTTPHeaderField:@"Authorization"];
-  if([_extraHeaders count])
+  if ([_extraHeaders count])
   {
-    for(NSString *header in _extraHeaders)
+    for (NSString *header in _extraHeaders)
       [request addValue:[_extraHeaders objectForKey:header] forHTTPHeaderField:header];
   }
   [request setHTTPBody:data];
   
   // Start the connection
   _connection = [NSURLConnection connectionWithRequest:request delegate:self];
-  if(_connection != nil) {
+  if (_connection != nil) {
     self.completionHandler = completionHandler;
   } else {
-    if(completionHandler) {
+    if (completionHandler) {
       completionHandler([self errorWithString:STRING_S3_BADCONNECTION]);
     }
     [self release];
@@ -266,16 +266,16 @@
   int len;
   size_t fileSize = 0;
   
-  if(![path length] || ![key length]) {
-    if(completionHandler) {
+  if (![path length] || ![key length]) {
+    if (completionHandler) {
       completionHandler([self errorWithString:STRING_S3_MISSINGPARAMS]);
     }
     return;
   }
   
   inputStream = [NSInputStream inputStreamWithFileAtPath:path];
-  if(!inputStream) {
-    if(completionHandler) {
+  if (!inputStream) {
+    if (completionHandler) {
       completionHandler([self errorWithString:STRING_S3_BADPATH]);
     }
     return;
@@ -285,7 +285,7 @@
   [self cancelCurrentRequest];
 
   // Don't allow keys to start with a /
-  if([key hasPrefix:@"/"]) {
+  if ([key hasPrefix:@"/"]) {
     key = [key substringFromIndex:1];
   }
 
@@ -293,8 +293,8 @@
   contentType = [self mimeType:path];
   CC_MD5_Init(&ctx);
   [inputStream open];
-  while((len = [inputStream read:buf maxLength:sizeof(buf)]) > 0) {
-    if(!fileSize) {
+  while ((len = [inputStream read:buf maxLength:sizeof(buf)]) > 0) {
+    if (!fileSize) {
       head[0] = buf[0];
       head[1] = buf[1];
     }
@@ -313,37 +313,38 @@
                    resource:key];
   
   // Setup the request
-  if(options & S3_HTTPS)
+  if (options & S3_HTTPS) {
     url = [NSURL URLWithString:[NSString stringWithFormat:S3_SECURE_URL, _bucket, key]];
-  else
+  } else {
     url = [NSURL URLWithString:[NSString stringWithFormat:S3_URL, _bucket, key]];
+  }
   request = [NSMutableURLRequest
              requestWithURL:url
              cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
              timeoutInterval:15.0];
   [request setHTTPMethod:@"PUT"];
   // Check for the gzip magic number and add the Content-Encoding header
-  if(options & S3_DETECT_GZIP && IS_GZIP(head)) {
+  if (options & S3_DETECT_GZIP && IS_GZIP(head)) {
     [request setValue:@"gzip" forHTTPHeaderField:@"Content-Encoding"];
   }
-  if(options & S3_NO_CACHE) {
+  if (options & S3_NO_CACHE) {
     [request setValue:@"no-cache" forKeyPath:@"Cache-Control"];
   }
-  if(options & S3_PERMANENT_CACHE) {
+  if (options & S3_PERMANENT_CACHE) {
     [request setValue:@"max-age=315360000" forKeyPath:@"Cache-Control"];
   }
-  if(options & S3_REDUCED_REDUNDANCY) {
+  if (options & S3_REDUCED_REDUNDANCY) {
     [request setValue:@"REDUCED_REDUNDANCY" forKeyPath:@"x-amz-storage-class"];
   }
-  if([contentType length]) {
+  if ([contentType length]) {
     [request setValue:contentType forHTTPHeaderField:@"Content-Type"];
   }
   [request setValue:[NSString stringWithFormat:@"%lu", fileSize] forHTTPHeaderField:@"Content-Length"];
   [request setValue:md5 forHTTPHeaderField:@"Content-MD5"];
   [request setValue:date forHTTPHeaderField:@"Date"];
   [request setValue:authorization forHTTPHeaderField:@"Authorization"];
-  if([_extraHeaders count]) {
-    for(NSString *header in _extraHeaders) {
+  if ([_extraHeaders count]) {
+    for (NSString *header in _extraHeaders) {
       [request addValue:[_extraHeaders objectForKey:header] forHTTPHeaderField:header];
     }
   }
@@ -351,10 +352,10 @@
   
   // Setup the connection
   _connection = [NSURLConnection connectionWithRequest:request delegate:self];
-  if(_connection != nil) {
+  if (_connection != nil) {
     self.completionHandler = completionHandler;
   } else {
-    if(completionHandler) {
+    if (completionHandler) {
       completionHandler([self errorWithString:STRING_S3_BADCONNECTION]);
     }
     [self release];
@@ -392,24 +393,24 @@
 
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
   _statusCode = [(NSHTTPURLResponse *)response statusCode];
-  if(_statusCode != 200) {
+  if (_statusCode != 200) {
     _responseData = [[NSMutableData alloc] init];
   }
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-  if(_responseData) {
+  if (_responseData) {
     [_responseData appendData:data];
   }
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
   _connection = nil;
-  if(_responseData) {
+  if (_responseData) {
     [_responseData release];
     _responseData = nil;
   }
-  if(_completionHandler) {
+  if (_completionHandler) {
     _completionHandler(error);
   }
   [self release];
@@ -420,7 +421,7 @@
 }
 
 - (void)connection:(NSURLConnection *)theConnection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
-  if([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+  if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
     [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
   }
   [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
@@ -428,21 +429,21 @@
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection {
   _connection = nil;
-  if(_statusCode != 200) {
+  if (_statusCode != 200) {
     NSString *message = nil;
     NSString *response = [[[NSString alloc] initWithData:_responseData encoding:NSUTF8StringEncoding] autorelease];
     NSRange start, end;
     start = [response rangeOfString:@"<message>" options:NSCaseInsensitiveSearch];
     end = [response rangeOfString:@"</message>" options:NSCaseInsensitiveSearch];
-    if(start.location != NSNotFound && end.location != NSNotFound) {
+    if (start.location != NSNotFound && end.location != NSNotFound) {
       message = [response substringWithRange:NSMakeRange(start.location + start.length, end.location - (start.location + start.length))];
     }
     [_responseData release];
     _responseData = nil;
-    if(_completionHandler) {
+    if (_completionHandler) {
       _completionHandler([self errorWithString:STRING_S3_HTTPERROR code:_statusCode]);
     }
-  } else if(_completionHandler) {
+  } else if (_completionHandler) {
     _completionHandler(nil);
   }
   [self release];
